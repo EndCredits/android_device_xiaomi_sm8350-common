7aa92adcc1f625b67438b8200c2514501bb678c7
EcrosoftXiao <xy19990903@gmail.com>
2022-03-17 02:38:26 +0800

    Build tinycompress

--- common.mk
+++ common.mk
@@ -37,7 +37,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audioadsprpcd \
     audio.r_submix.default \
-    audio.usb.default
+    audio.usb.default \
+    libtinycompress
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
