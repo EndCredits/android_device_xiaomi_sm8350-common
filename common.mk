98211166a4bfb1d7dd8da073d79c888f67554974
Michael Bestas <mkbestas@lineageos.org>
2022-07-06 21:09:12 +0300

    sm8350-common: Fix disabling A2DP offload
    Change-Id: I49c717a19afb2491b6280670fdbfe8765813a4dc
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common.mk
+++ common.mk
@@ -47,6 +47,7 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration_a2dp_offload_disabled.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_a2dp_offload_disabled.xml \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_hearing_aid_audio_policy_configuration.xml
 
