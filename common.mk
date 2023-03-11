002e799e78e1460e8c497634d9142271066d3d47
EndCredits <alicization.han@gmail.com>
2023-03-11 19:41:33 +0800

    sm8350-common: Build libldacBT_bco package

--- common.mk
+++ common.mk
@@ -107,6 +107,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     com.dsi.ant@1.0.vendor \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
