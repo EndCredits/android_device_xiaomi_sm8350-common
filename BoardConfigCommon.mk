6a159701fa87f9be0776d74d7526249e4acabd49
EndCredits <endcredits@crepuscular-aosp.icu>
2023-04-08 23:23:34 +0800

    sm8350-common: Initial bring up for DerpFest
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,7 +76,7 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/hidl/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml \
+    vendor/derp/config/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
