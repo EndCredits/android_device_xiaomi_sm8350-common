6c3edd13c17e7571af5231e74733caa0344b9db5
EndCredits <endcredits@crepuscular-aosp.icu>
2023-05-11 23:56:02 +0800

    Revert "sm8350-common: Build android.hardware.graphics.allocator@3.0-service"
    This reverts commit 42dc4594a85fcee1ead2c98a436a9c0605e91973.

--- common.mk
+++ common.mk
@@ -162,7 +162,6 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@3.0-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
