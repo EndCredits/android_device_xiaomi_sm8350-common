4168550a8c1dc63b330617f6a5f70091e82c9878
EndCredits <endcredits@crepuscular-aosp.icu>
2023-05-14 23:56:04 +0800

    Revert "sm8350-common: Drop livedisplay temporarily"
    This reverts commit 1d39ceccd7a8c748c90908bbd61cc1832bb1e317.

--- common.mk
+++ common.mk
@@ -307,6 +307,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+ifneq ("$(wildcard hardware/lineage/livedisplay)", "")
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.1-service.xiaomi_sm8350
+endif
+
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
