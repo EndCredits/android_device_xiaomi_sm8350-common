e16f5880f518545d8f1d6caebec80dfe72b79f48
EndCredits <endcredits@crepuscular-aosp.icu>
2023-05-22 23:39:00 +0800

    sm8350-common: Mirror shima powerhint from lahaina
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- common.mk
+++ common.mk
@@ -437,7 +437,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public libraries
 PRODUCT_COPY_FILES += \
