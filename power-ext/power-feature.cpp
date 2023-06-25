8b14396941e1c32d168d5e050838eb05d7da8d3e
EndCredits <endcredits@crepuscular-aosp.icu>
2023-06-25 08:00:23 +0800

    sm8350-common: Switch to AOSPA common power HAL
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

---
+++ power-ext/power-feature.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <aidl/vendor/aospa/power/BnPowerFeature.h>
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <sys/ioctl.h>
+
+// defines from drivers/input/touchscreen/xiaomi/xiaomi_touch.h
+#define SET_CUR_VALUE 0
+#define Touch_Doubletap_Mode 14
+
+#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
+#define TOUCH_ID 0
+#define TOUCH_MAGIC 0x5400
+#define TOUCH_IOC_SETMODE TOUCH_MAGIC + SET_CUR_VALUE
+
+namespace aidl {
+namespace vendor {
+namespace aospa {
+namespace power {
+
+bool setDeviceSpecificFeature(Feature feature, bool enabled) {
+    switch (feature) {
+        case Feature::DOUBLE_TAP: {
+            int fd = open(TOUCH_DEV_PATH, O_RDWR);
+            int arg[3] = {TOUCH_ID, Touch_Doubletap_Mode, enabled ? 1 : 0};
+            ioctl(fd, TOUCH_IOC_SETMODE, &arg);
+            close(fd);
+            return true;
+        }
+        default:
+            return false;
+    }
+}
+
+}  // namespace power
+}  // namespace aospa
+}  // namespace vendor
+}  // namespace aidl
