fe5f28d7ba62ea0a4075ebb082363731ff7c2259
EndCredits <endcredits@crepuscular-aosp.icu>
2023-06-26 15:37:24 +0800

    sm8350-common: Switch to common usb components
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- common.mk
+++ common.mk
@@ -297,6 +297,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     nfc \
     overlay \
     telephony \
+    usb \
     perf
 
 # Recovery
@@ -345,14 +346,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator
 PRODUCT_PACKAGES += \
