c60d4edcee9593ce5564b609c5227fd927f39551
EndCredits <endcredits@crepuscular-aosp.icu>
2023-06-28 19:53:10 +0800

    sm8350-common: Switch to prebuilt megface libs
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- common.mk
+++ common.mk
@@ -89,10 +89,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
-PRODUCT_PACKAGES += \
-    libMegviiFacepp-0.5.2 \
-    libmegface_vendor
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
 
