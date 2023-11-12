b7526b5f824fa576d41006385375b440b69d4119
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-11-12 19:44:29 +0100

    sm8350-common: Move to health AIDL services
    Change-Id: Ibadf22d432791551c16d7f4e386b470574c390f8
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- common.mk
+++ common.mk
@@ -253,7 +253,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.example \
+    android.hardware.health-service.example_recovery
 
 PRODUCT_PACKAGES += \
     libhidltransport.vendor \
