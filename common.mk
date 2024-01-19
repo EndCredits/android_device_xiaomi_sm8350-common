9c49b37604fa7562afb244897baa1fbaddb6a20a
EndCredits <endcredits@crepuscular-aosp.icu>
2024-01-19 20:45:07 +0800

    Revert "sm8350-common: Switch to common QTI vibrator HAL"
    This reverts commit 69d3f34b637907d97985bd3bd6fe52bc7b17ed49.

--- common.mk
+++ common.mk
@@ -561,10 +561,10 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_sm8350
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 # vndservicemanager has been removed from API30 devices (aosp/1235751)
