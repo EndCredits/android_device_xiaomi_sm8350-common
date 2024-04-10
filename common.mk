3e430b6440a6751f48c399728af99db8bc7899cc
EndCredits <endcredits@crepuscular-aosp.icu>
2024-04-10 12:21:40 +0800

    Revert "sm8350-common: livedisplay: Uprev to 2.1"
    This reverts commit beffaa7cc2dffa551b878a46070e062ff3eb08ba.

--- common.mk
+++ common.mk
@@ -309,7 +309,7 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.1-service.xiaomi_sm8350
+    vendor.lineage.livedisplay@2.0-service.xiaomi_sm8350
 
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
