3f77b9ee29b2ade6a43e8519e36d919ef49eca93
EndCredits <endcredits@crepuscular-aosp.icu>
2024-04-10 12:21:38 +0800

    Revert "sm8350-common: compile livedisplay only if dependencies are satisfied"
    This reverts commit 0fc18ae9c8091bc53bf99a3d85c4a23ba4abc380.

--- common.mk
+++ common.mk
@@ -306,12 +306,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-ifneq ("$(wildcard hardware/lineage/livedisplay)", "")
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
     vendor.lineage.livedisplay@2.1-service.xiaomi_sm8350
-endif
 
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
