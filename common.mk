f754fbc3426095d41a9efb3e4de7f3d1c9d76e89
EndCredits <endcredits@crepuscular-aosp.icu>
2024-09-05 22:55:06 +0800

    sm8350-common: Drop livedisplay
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- common.mk
+++ common.mk
@@ -313,10 +313,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
