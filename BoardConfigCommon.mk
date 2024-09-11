993f991cde3b9f958e35c2979b4bea4daa8c3f98
EndCredits <endcredits@crepuscular-aosp.icu>
2024-09-11 12:58:52 +0800

    Revert "sm8350-common: Update blobs to haydn V816.0.1.0.UKKCNXM"
    This reverts commit 8043949cdd7da2d024f96b462b36446d84c5266e.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -190,7 +190,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-04-01
+VENDOR_SECURITY_PATCH := 2023-07-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
