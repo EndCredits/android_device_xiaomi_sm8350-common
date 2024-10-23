eb335d4af860a8e48a1963bd4dc9f732a326a479
EndCredits <endcredits@crepuscular-aosp.icu>
2024-10-24 07:24:54 +0800

    sm8350-common: Workaround libmegface conflict
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,6 +43,7 @@ function lib_to_package_fixup_vendor_variants() {
     case "$1" in
         com.qualcomm.qti.dpm.api@1.0 | \
             libmmosal | \
+            libmegface | \
             vendor.qti.hardware.wifidisplaysession@1.0 | \
             vendor.qti.imsrtpservice@3.0 | \
             vendor.qti.diaghal@1.0)
