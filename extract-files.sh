bffcb19e185097e5ca789825ae322ae530d2cdb9
EndCredits <endcredits@crepuscular-aosp.icu>
2024-10-24 13:34:54 +0800

    sm8350-common: Link libcrypto-v33 to mediadrm blob
     * Provide symbol "CBS_init"
    
     * This fixed ELF Check
    
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- extract-files.sh
+++ extract-files.sh
@@ -93,7 +93,8 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
-        vendor/lib64/libwvhidl.so)
+        vendor/lib64/libwvhidl.so \
+        |vendor/lib64/mediadrm/libwvdrmengine.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
         *)
